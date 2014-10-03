67419ebb499614c4b4d78260001bbc09548a8705
Ross Girshick <ross.girshick@gmail.com>
2014-10-03 13:50:22 -0700

    function to add padding and optionally output DPM style feature pyramids

---
+++ deep_pyramid_add_padding.m
@@ -0,0 +1,32 @@
+function pyra = deep_pyramid_add_padding(pyra, padx, pady, uniform_output)
+% pyra = deep_pyramid_add_padding(pyra, padx, pady, uniform_output)
+%
+% pyra: a pyramid computed using deep_pyramid.m
+% padx, pady: amount of extra padding to add around each pyramid level
+% uniform_output: true => each pyramid level is embedded in the upper-left
+%     corner of each 3D plane inside a 4D array;
+%     false => each pyramid level is stored as a 3D array with the height
+%     and width of that level (same as in the DPM HOG feature pyramid code)
+
+pyra.padx = padx;
+pyra.pady = pady;
+
+if ~exist('uniform_output', 'var') || isempty(uniform_output)
+  uniform_output = false;
+end
+
+if uniform_output
+  pyra.feat = padarray(pyra.feat, [pady padx 0 0], 0);
+else
+  % This mimics the output of the DPM featpyramid.m code
+  feat = pyra.feat;
+  pyra.feat = cell(pyra.num_levels, 1);
+  for i = 1:pyra.num_levels
+    % crop out feat map levels
+    pyra.feat{i} = feat(1:pyra.level_sizes(i, 1), ...
+                        1:pyra.level_sizes(i, 2), :, i);
+    % add padding
+    pyra.feat{i} = padarray(pyra.feat{i}, [pady padx 0], 0);
+  end
+end
+
